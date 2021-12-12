02f9864012d85e95a68b8bc72a8ff2258208da7f
Vo Huy Nguyen <huynguyenvo2001@gmail.com>
2021-12-12 10:31:24 +0700

    index view

--- app/main.py
+++ app/main.py
@@ -10,6 +10,27 @@ from app import login, app
 def home():
     return render_template('index.html')
 
+#login method for employee
+@app.route('/nhanvien_login', methods=['get', 'post'])
+def nhanvien_login():
+    error_msg = ""
+    if request.method.__eq__('POST'):
+        try:
+            username = request.form['username']
+            password = request.form['password']
+
+            user = utils.check_user(username=username, password=password, role=UserRole.EMPLOYEE)
+            if user:
+                login_user(user=user)
+
+                return render_template('employee.html')
+            else:
+                error_msg = "Chuong trinh dang co loi! Vui long quay lai sau!"
+
+        except Exception as ex:
+            error_msg = str(ex)
+
+    return render_template('index.html', error_msg=error_msg)
 
 # Login method for admin, page admin
 @app.route('/admin-login', methods=['post'])
@@ -30,7 +51,11 @@ def admin_login():
 def load_user(user_id):
     return utils.get_user_by_id(user_id=user_id)
 
+@app.route('/logout')
+def nhanvien_logout():
+    logout_user()
 
+    return redirect(url_for('nhanvien_login'))
 
 
 
